8beb57a2ae4fede63abc03ce63959cb200128a26
Leonardo merchan <leonardo.merchan.1992@gmail.com>
2016-10-31 18:05:19 +0000

    correcion error prueba

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,13 +2,18 @@ class Image < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
   
-  validate :picture.size
-  private
-  
-  def picture_size
-    if picture.size > 5.megabytes
-    errors.add(:picture,"should be less than 5MB")
-    end
-  end
+  validate :picture_size
+
+private
+
+def picture_size
+
+if picture.size > 5.megabytes
+
+errors.add(:picture, "should be less than 5MB")
+
+end
+
+end
   
 end
